27e871e0f20859fd66c08022321ca3f7b2fb2528
gudmunduro <goh140400@gmail.com>
2018-03-14 21:16:22 +0000

    breytingar

---
+++ Game/Config/index.js
@@ -0,0 +1,19 @@
+﻿
+function config() {
+    return {
+        type: Phaser.AUTO,
+        width: 800,
+        height: 600,
+        physics: {
+            default: 'arcade',
+            arcade: {
+                gravity: { y: 200 }
+            }
+        },
+        scene: {
+            preload: preload,
+            create: create,
+            update: update
+        }
+    };
+}
---
+++ Game/main.js
@@ -0,0 +1,18 @@
+// import { config } from "./Config";
+
+var game = new Phaser.Game(config());
+
+function preload()
+{
+
+}
+
+function create()
+{
+    this.add.text(10, 10, "texti");
+}
+
+function update()
+{
+
+}
